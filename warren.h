b35d9893712d2d044b70c883956b8838836f1fa9
Warren <Warren@LAPTOP-QAV3KMP9>
2021-12-21 16:09:46 +0800

    添加项目文件。

---
+++ warren.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "resource.h"
+#define _CRT_SECURE_NO_WARNINGS  //关闭vs对早期C库函数的安全警告
+#include <stdio.h>
+#include <iostream>
+
+#include <cstdio>
+
+#include <fstream>
+
+#include <algorithm>
+
+#include <cmath>
+
+#include <deque>
+
+#include <vector>
+
+#include <queue>
+
+#include <string>
+
+#include <cstring>
+
+#include <map>
+
+#include <stack>
+
+#include <set> using namespace std;
